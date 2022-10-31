82dce4cb22b919c99885cab60438826429e2d10d
James Jilhaney <notail2111@gmail.com>
2022-10-31 21:47:10 +0800

    Will now create Certificates folder if it doesn't exist yet

--- certificate.py
+++ certificate.py
@@ -78,8 +78,23 @@ directory = {}
 # gets the length of the rows of the file to limit the iteration below
 row_count = ws.max_row
 
+# Check if Certificate directory exists and create it if it does not
+hasCertificateDir = False
+currentpath = os.getcwd()
+temp = os.scandir(currentpath)
+for entry in temp:
+    if entry.is_dir():
+        if entry.name == "Certificates":
+            hasCertificateDir = True
+
+if (hasCertificateDir == False):
+    print('Cannot find Certificate directory. Creating it now.')
+    os.mkdir(currentpath + "/Certificates")
+
 # iterates over all the rows and returns a tuple
 for row in ws.iter_rows(min_row=2, max_row=row_count):
+   # if path == CerticatesPath:
+
     # open the certificate template
     image = Image.open('participants.png')
     draw = ImageDraw.Draw(image)
